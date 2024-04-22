52bb52f6b17d54397b9b7111a5d266e296c33569
Rémi Desbordes <remi.desbordes@epitech.eu>
2024-04-22 18:14:42 +0200

    [ADD] Hello World

---
+++ src/main/java/com/example/Friend/HelloController.java
@@ -0,0 +1,15 @@
+package com.example.Friend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    
+    @RequestMapping("/")
+    public String HelloWorld() {
+        return ("Hello World !");
+    }
+}
---
+++ src/test/java/com/example/Friend/FriendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Friend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
